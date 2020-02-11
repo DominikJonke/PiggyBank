8714217209063ad832e13ce05885e7b2e3cbff7c
D J <dominik.jonke@outlook.com>
2020-02-11 13:09:30 +0100

    New static method (Calculator)

---
+++ PiggyBank/PiggyBank/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyBank
+{
+    static class Calculator
+    {
+        public static double CalculateMoney()
+        {
+            int fifty = 0;
+            int one = 0;
+            int two = 0;
+            double sum = 0;
+
+            Console.WriteLine("Fifty Cent: ");
+            fifty = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("One Euro: ");
+            one = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Two Euro: ");
+            two = Convert.ToInt32(Console.ReadLine());
+
+            sum = fifty * 0.5;
+            sum += one;
+            sum += two * 2;
+
+            return sum;
+        }
+    }
+}
---
+++ PiggyBank/PiggyBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t\t Piggy Bank");
+            Console.WriteLine();
+            Console.WriteLine($"You have {Calculator.CalculateMoney()} Euro");
+        }
+    }
+}
